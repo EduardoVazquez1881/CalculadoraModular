bde11769e8830e9aaa6e9ce5572a0b56ca625671
eduardovazquez1881 <eduardovz1881@gmail.com>
2025-09-27 16:32:42 -0700

    Caucluladora basica sin diseño

---
+++ main.js
@@ -0,0 +1,89 @@
+document.querySelectorAll(".teclado button").forEach(btn =>{
+    btn.addEventListener("click", () =>{
+        const valor = btn.textContent;
+        Main(valor);
+
+    })
+})
+
+let operador;
+let listIzq = [];
+let listDer = [];
+
+function Display(izq, op, der){
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = `${izq} ${op ? op : ""} ${der}`;
+
+}
+
+function ListIzq(){
+    return listIzq.join("");
+}
+
+function ListDer(){
+    return listDer.join("");
+}
+
+function Operarcion(izq, op, der){
+    if(op == "+") return izq + der;
+    if(op == "-") return izq - der;
+    if(op == "*") return izq * der;
+    if(op == "/") return izq / der;
+}
+
+
+function Main(dataKeyboard){
+    switch(dataKeyboard){
+        case '+':{
+            operador = "+";
+            Display(ListIzq(), operador, ListDer());
+            break;
+        }
+        case '-':{
+            operador = "-";
+            Display(ListIzq(), operador, ListDer());
+            break;
+        }
+        case '*':{
+            operador = "*";
+            Display(ListIzq(), operador, ListDer());
+            break;
+        }
+        case '/':{
+            operador = "/";
+            Display(ListIzq(), operador, ListDer());
+            break;
+        }
+        case '=':{
+            let izq = parseInt(listIzq.join(""));
+            let der = parseInt(listDer.join(""));
+            document.getElementById("resultado").innerText = Operarcion(izq, operador, der);
+            break;
+        }
+        case 'C':{
+            listDer = [];
+            listIzq = [];
+            operador = null;
+            Display("0", "", "");
+            break;
+        }
+
+        default:{
+            const contenido = document.getElementById("resultado").textContent;
+
+
+            if(contenido.includes("+") || contenido.includes("-") || contenido.includes("*") || contenido.includes("/"))
+            {
+                listDer.push(dataKeyboard);
+                Display(ListIzq(), operador, ListDer());
+            }
+            else
+            {
+                listIzq.push(dataKeyboard);
+                Display(ListIzq(), operador, "");
+            }
+        }   
+
+        
+    }    
+}
